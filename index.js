a60b0bb4a53544b466257a934748e2bd489b5294
uilmi <uilmi@na.edu>
2021-09-13 19:18:08 +0700

    express nodemon pg sequelize are added

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+const LOCALHOST = 'http://localhost';
+
+
+app.get('/', (req, res) => {
+    res.send('Ready!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Sever ready -> ${LOCALHOST}:${PORT}`);
+})
